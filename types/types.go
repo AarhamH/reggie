0855c3c1fa4ec3f3a1c615fe418569b44b15a165
AarhamH <ahaider@cayenta.com>
2024-11-22 13:52:22 -0800

    Define parser token constants

---
+++ types/types.go
@@ -0,0 +1,10 @@
+package types
+
+const (
+	group      uint8 = iota
+	bracket    uint8 = iota
+	or         uint8 = iota
+	repeat     uint8 = iota
+	literal    uint8 = iota
+	groupUncap uint8 = iota
+)
